28a1fb99d082eaa9dc929b93ebb906965b8c4321
tawing101 <98728065+tawing101@users.noreply.github.com>
2026-04-10 18:25:25 +0800

    Update trade.ts

--- src/roo/schedule/trade.ts
+++ src/roo/schedule/trade.ts
@@ -13,19 +13,18 @@ export const trades = [
 ] satisfies Trade[];
 
 export const getTradeDuration = (value: Trade): Duration => {
-	// Store refreshes don't have a duration, so we return an empty object
 	return {};
 };
 
 export const getTradeTime = (value: Trade): ScheduleTime => {
 	switch (value) {
 		case Trade.FirstStoreRefresh:
-			return { hours: 12, minutes: 0 }; // Alerts at 12:50 PM PH / 1:00 PM PH
+			return { hours: 4, minutes: 0 }; // 04:00 In-game (Starts at 5:00 AM PH)
 
 		case Trade.SecondStoreRefresh:
-			return { hours: 16, minutes: 0 }; // Alerts at 4:50 PM PH / 5:00 PM PH
+			return { hours: 12, minutes: 0 }; // 12:00 In-game (Starts at 1:00 PM PH)
 
 		case Trade.ThirdStoreRefresh:
-			return { hours: 20, minutes: 0 }; // Alerts at 8:50 PM PH / 9:00 PM PH
+			return { hours: 20, minutes: 0 }; // 20:00 In-game (Starts at 9:00 PM PH)
 	}
 };
